526878aaec389a0773faef7ee7436ee060757dbf
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-12-21 10:22:47 +0100

    mise en place date picker

--- src/components/Ui/Form.js
+++ src/components/Ui/Form.js
@@ -1,10 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
+// import ReactDropdown from 'react-dropdown';
+
+import allData from '@/data/states'
+import { useDispatch } from 'react-redux';
+import { addEmployee } from '@/feature/employeeSlice.slice';
+import ReactDropdown from 'react-dropdown';
+
 
 const Form = () => {
-    return (
-        <div className='form'>
 
-        </div>
+    const [credentials, setCredentials] = useState({
+        firstName: '',
+        lastName: '',
+        birthDate: '',
+        startDate: '',
+        street: '',
+        city: '',
+        states: '',
+        zipcode: '',
+        department: '',
+    })
+
+    const dispatch = useDispatch()
+
+    const onChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleEdit = (e) => {
+        e.preventDefault()
+        const employee = {
+            // firstName,
+            // lastName,
+            // department,
+            // startDate,
+            // birthDate,
+            // street,
+            // city,
+            // states,
+            // zipcode,
+        }
+
+        dispatch(addEmployee(employee))
+    }
+
+    return (
+        <form action="">
+            <h2>Create Employee</h2>
+
+            <div className='formData'>
+                <label className="form-label" for="firstName">First Name: </label>
+                <input className="form-control" type="text" id="first" name="firstName" placeholder='Firstname' onChange={onChange} />
+            </div>
+
+            <div className='formData'>
+                <label className="form-label" for="lastName">Last Name: </label>
+                <input className="form-control" type="text" id="last" name="lastName" placeholder='Lastname' onChange={onChange} />
+            </div>
+
+            <div className='formData'>
+                <label className="form-label" for="birth">Date of Birth: <ReactDatePicker
+                    selected={credentials.birthDate}
+                    dateFormat='dd/mm/yyyy'
+                    onChange={onChange}
+                /> </label>
+            </div>
+
+            <div className='formData'>
+                <label className="form-label" for="start">Start Date:<ReactDatePicker
+                    selected={credentials.startDate}
+                    dateFormat='dd/mm/yyyy'
+                    onChange={onChange}
+                /> </label>
+
+            </div>
+
+            <fieldset className='adress-area'>
+
+                <div className='formData'>
+                    <label className="form-label" for="street">Street: </label>
+                    <input className="form-control" type="text" id="last" name="street" placeholder='Street' onChange={onChange} />
+                </div>
+
+                <div className='formData'>
+                    <label className="form-label" for="city">City: </label>
+                    <input className="form-control" type="text" id="last" name="city" placeholder='City' onChange={onChange} />
+                </div>
+
+                <div className='formData'>
+                    <label className="form-label"> State </label>
+
+                </div>
+
+                <div className='formData'>
+                    <label className="form-label" for="zipcode">Zip Code: </label>
+                    <input className="form-control" type="number" id="last" name="zipcode" placeholder='Zip Code' onChange={onChange} />
+                </div>
+
+
+            </fieldset>
+
+        </form>
     );
 };
 
--- src/pages/CreateEmployee.js
+++ src/pages/CreateEmployee.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Form from '../components/Ui/Form';
 
 const CreateEmployee = () => {
+
+
+
     return (
-        <div className='title'>
+        <div className='create-employee'>
             <h1>HRnet</h1>
+            <Form />
         </div>
     );
 };
