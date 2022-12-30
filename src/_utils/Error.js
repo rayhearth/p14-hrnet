545316801da14d93493ca41e1016bdac3978d96f
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-12-30 13:16:43 +0100

    correction 404

--- src/_utils/Error.js
+++ src/_utils/Error.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div className='error'>
-
+        <div className='pageError-404'>
+            <h1>404</h1>
+            <p>La page que vous demandez n'existe pas.</p>
+            <Link to='/'>Retourner sur la page d'accueil</Link>
         </div>
     );
 };
